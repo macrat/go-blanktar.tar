b9f70d111d6d4e14ca6492c078e7a1c9e6467259
MacRat <m@crat.jp>
2019-07-17 16:09:06 +0900

    Updated

---
+++ block.go
@@ -0,0 +1,474 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+    "strings"
+    "path"
+	"fmt"
+	"io"
+	"os"
+	"time"
+    "errors"
+)
+
+var (
+    PropertyOverflow = errors.New("property overflow")
+    NameTooLong = errors.New("file name is too long")
+)
+
+func toHumanReadable(i uint64) string {
+	switch {
+	case i < 1<<10:
+		return fmt.Sprintf("%dB", i)
+	case i < 1<<20:
+		return fmt.Sprintf("%.2fKB", float64(i)/(1<<10))
+	case i < 1<<30:
+		return fmt.Sprintf("%.2fMB", float64(i)/(1<<20))
+	default:
+		return fmt.Sprintf("%.2fGB", float64(i)/(1<<30))
+	}
+}
+
+type String155 [155]byte
+
+func NewString155(s string) (String155, error) {
+	var r String155
+
+    b := []byte(s)
+    if len(b) - 1 >= 155 {
+        return r, PropertyOverflow
+    }
+
+	copy(r[:], b)
+	return r, nil
+}
+
+func (s String155) String() string {
+	return strings.TrimRight(string(s[:]), "\u0000")
+}
+
+type String100 [100]byte
+
+func NewString100(s string) (String100, error) {
+	var r String100
+
+    b := []byte(s)
+    if len(b) - 1 >= 100 {
+        return r, PropertyOverflow
+    }
+
+	copy(r[:], b)
+	return r, nil
+}
+
+func (s String100) String() string {
+	return strings.TrimRight(string(s[:]), "\u0000")
+}
+
+type String32 [32]byte
+
+func NewString32(s string) (String32, error) {
+	var r String32
+
+    b := []byte(s)
+    if len(b) - 1 >= 32 {
+        return r, PropertyOverflow
+    }
+
+	copy(r[:], b)
+	return r, nil
+}
+
+func (s String32) String() string {
+	return strings.TrimRight(string(s[:]), "\u0000")
+}
+
+type String8 [8]byte
+
+func NewString8(s string) (String8, error) {
+	var r String8
+
+    b := []byte(s)
+    if len(b) - 1 >= 8 {
+        return r, PropertyOverflow
+    }
+
+	copy(r[:], b)
+	return r, nil
+}
+
+func (s String8) String() string {
+	return strings.TrimRight(string(s[:]), "\u0000")
+}
+
+type Mode [8]byte
+
+func NewMode(mode os.FileMode) Mode {
+	var i uint32
+
+	i = uint32(mode & os.ModePerm)
+	if mode&os.ModeSetuid != 0 {
+		i |= 04000
+	}
+	if mode&os.ModeSetgid != 0 {
+		i |= 02000
+	}
+
+	var m Mode
+	copy(m[:], fmt.Sprintf("%07o", i))
+	return m
+}
+
+func (m Mode) FileMode() os.FileMode {
+	var i os.FileMode
+
+	fmt.Sscanf(string(m[3:]), "%o", &i)
+
+	if i&04000 != 0 {
+		i |= os.ModeSetuid
+	}
+	if i&02000 != 0 {
+		i |= os.ModeSetgid
+	}
+
+	return os.FileMode(i)
+}
+
+func (m Mode) String() string {
+	return fmt.Sprintf("%o", m.FileMode())
+}
+
+type Size [12]byte
+
+func NewSize(size uint64) Size {
+	var s Size
+	copy(s[:], []byte(fmt.Sprintf("%07o", size)))
+	return s
+}
+
+func (s Size) Int() uint64 {
+	var i uint64
+	fmt.Sscanf(string(s[:]), "%o", &i)
+	return i
+}
+
+func (s Size) String() string {
+	return toHumanReadable(s.Int())
+}
+
+type Timestamp [12]byte
+
+func NewTimestamp(t time.Time) Timestamp {
+    n := t.Unix()
+    if n < 0 {
+        n = 0
+    }
+
+    var x Timestamp
+    copy(x[:], []byte(fmt.Sprintf("%011o", n)))
+    return x
+}
+
+func (t Timestamp) Time() time.Time {
+	var i int64
+	fmt.Sscanf(string(t[:]), "%o", &i)
+	return time.Unix(i, 0)
+}
+
+func (t Timestamp) String() string {
+	return fmt.Sprint(t.Time())
+}
+
+type CheckSum [8]byte
+
+func NewCheckSum(checksum int64) CheckSum {
+	var c CheckSum
+	copy(c[:], []byte(fmt.Sprintf("%07o", checksum)))
+	return c
+}
+
+func (c CheckSum) Int() int64 {
+	var i int64
+	fmt.Sscanf(string(c[:]), "%o", &i)
+	return i
+}
+
+func (c CheckSum) String() string {
+	return fmt.Sprintf("0x%016X", c.Int())
+}
+
+type TypeFlag byte
+
+const (
+	REGTYPE  TypeFlag = '0'
+	AREGTYPE TypeFlag = '\n'
+	LINKTYPE TypeFlag = '1'
+	SYMTYPE  TypeFlag = '2'
+	CHRTYPE  TypeFlag = '3'
+	BLKTYPE  TypeFlag = '4'
+	DIRTYPE  TypeFlag = '5'
+	FIFOTYPE TypeFlag = '6'
+	CONTTYPE TypeFlag = '7'
+)
+
+func NewTypeFlag(mode os.FileMode) TypeFlag {
+    switch {
+    case mode & os.ModeSymlink != 0:
+        return SYMTYPE
+    case mode & os.ModeCharDevice != 0:
+        return CHRTYPE
+    case mode & os.ModeDevice != 0:
+        return BLKTYPE
+    case mode & os.ModeDir != 0:
+        return DIRTYPE
+    case mode & os.ModeNamedPipe != 0:
+        return FIFOTYPE
+    default:
+        return REGTYPE
+    }
+}
+
+func (t TypeFlag) FileMode() os.FileMode {
+	switch t {
+	case SYMTYPE:
+		return os.ModeSymlink
+	case CHRTYPE:
+		return os.ModeCharDevice
+	case BLKTYPE:
+		return os.ModeDevice
+	case DIRTYPE:
+		return os.ModeDir
+	case FIFOTYPE:
+		return os.ModeNamedPipe
+	default:
+		return os.FileMode(0)
+	}
+}
+
+func (t TypeFlag) String() string {
+	switch t {
+	case REGTYPE:
+		return "regular file"
+	case AREGTYPE:
+		return "regular file (already archived)"
+	case LINKTYPE:
+		return "link"
+	case SYMTYPE:
+		return "symbolic link"
+	case CHRTYPE:
+		return "character device"
+	case BLKTYPE:
+		return "block device"
+	case DIRTYPE:
+		return "directory"
+	case FIFOTYPE:
+		return "fifo special file"
+	case CONTTYPE:
+		return "reserved"
+	default:
+		return "unknown"
+	}
+}
+
+type Magic [6]byte
+
+func NewMagic() Magic {
+	var m Magic
+	copy(m[:], "ustar")
+	return m
+}
+
+func (m Magic) String() string {
+	return string(m[:])
+}
+
+type Version [2]byte
+
+func NewVersion() Version {
+	var v Version
+	v[0] = '0'
+	v[1] = '0'
+	return v
+}
+
+type Block interface {
+    IsHeader() bool
+    IsFooter() bool
+	WriteTo(io.Writer) error
+}
+
+type ID [8]byte
+
+func NewID(id uint32) ID {
+	var i ID
+	copy(i[:], []byte(fmt.Sprintf("%07o", id)))
+	return i
+}
+
+func (id ID) Int() uint32 {
+	var i uint32
+	fmt.Sscanf(string(id[:]), "%o", &i)
+	return i
+}
+
+func (id ID) String() string {
+	return fmt.Sprintf("%d", id.Int())
+}
+
+type HeaderBlock struct {
+	Name      String100
+	Mode      Mode
+	UID       ID
+	GID       ID
+	Size      Size
+	Modified  Timestamp
+	CheckSum  CheckSum
+	TypeFlag  TypeFlag
+	LinkName  String100
+	Magic     Magic
+	Version   Version
+	UserName  String32
+	GroupName String32
+	DevMajor  String8
+	DevMinor  String8
+	Prefix    String155
+	Padding   [12]byte
+}
+
+func NewHeaderBlock(info os.FileInfo) (HeaderBlock, error) {
+    name := info.Name()
+    prefix := ""
+
+    for len([]byte(name)) - 1 >= 100 {
+        xs := strings.SplitN(name, "/", 2)
+        prefix = path.Join(prefix, xs[0])
+        name = xs[1]
+    }
+
+    n, err := NewString100(name)
+    if err != nil {
+        return HeaderBlock{}, NameTooLong
+    }
+    p, err := NewString155(prefix)
+    if err != nil {
+        return HeaderBlock{}, NameTooLong
+    }
+
+    h := HeaderBlock{
+		Name:     n,
+		Mode:     NewMode(info.Mode()),
+        Modified: NewTimestamp(info.ModTime()),
+		TypeFlag: NewTypeFlag(info.Mode()),
+		Magic:    NewMagic(),
+		Version:  NewVersion(),
+		Prefix:   p,
+	}
+
+    h.CheckSum = NewCheckSum(h.CalcSum())
+
+    return h, nil
+}
+
+func (h HeaderBlock) IsHeader() bool {
+    return h.calcTotal() > 0
+}
+
+func (h HeaderBlock) IsFooter() bool {
+    return h.calcTotal() == 0
+}
+
+func (h HeaderBlock) ContentBlockNum() uint64 {
+	if h.TypeFlag != REGTYPE && h.TypeFlag != CONTTYPE {
+        return 0
+    }
+	return (h.Size.Int() + 511) / 512
+}
+
+func (h HeaderBlock) WriteTo(w io.Writer) error {
+	return binary.Write(w, binary.BigEndian, h)
+}
+
+func (h HeaderBlock) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	h.WriteTo(buf)
+	return buf.Bytes()
+}
+
+func (h HeaderBlock) calcTotal() int64 {
+	var sum int64
+
+	for _, x := range h.Bytes() {
+		sum += int64(x)
+	}
+
+	return sum
+}
+
+func (h HeaderBlock) CalcSum() int64 {
+	sum := h.calcTotal()
+
+	for _, x := range [8]byte(h.CheckSum) {
+		sum -= int64(x)
+	}
+	sum += 0x30*6 - 0x20
+
+	return sum
+}
+
+func (h HeaderBlock) Validate() bool {
+	return h.CalcSum() == h.CheckSum.Int()
+}
+
+type ContentBlock [512]byte
+
+func (c ContentBlock) IsHeader() bool {
+    return false
+}
+
+func (c ContentBlock) IsFooter() bool {
+    return false
+}
+
+func (c ContentBlock) WriteTo(w io.Writer) error {
+	_, err := w.Write(c[:])
+	return err
+}
+
+type FooterBlock [1024]byte
+
+func (f FooterBlock) IsHeader() bool {
+    return false
+}
+
+func (f FooterBlock) IsFooter() bool {
+    return true
+}
+
+func (f FooterBlock) WriteTo(w io.Writer) error {
+	_, err := w.Write(f[:])
+	return err
+}
+
+type BlockArray []Block
+
+func (b BlockArray) IsHeader() bool {
+    return false
+}
+
+func (b BlockArray) IsFooter() bool {
+    if len(b) == 0 {
+        return false
+    }
+
+    return b[len(b)-1].IsFooter()
+}
+
+func (b BlockArray) WriteTo(w io.Writer) error {
+    for _, b := range b {
+        if err := b.WriteTo(w); err != nil {
+            return err
+        }
+    }
+    return nil
+}
--- main.go
+++ main.go
@@ -3,435 +3,149 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+    "path"
+    "strings"
 	"fmt"
 	"io"
 	"os"
 	"time"
+    "net/http"
 )
 
-func toHumanReadable(i int64) string {
-	switch {
-	case i < 1<<10:
-		return fmt.Sprintf("%dB", i)
-	case i < 1<<20:
-		return fmt.Sprintf("%.2fKB", float64(i)/(1<<10))
-	case i < 1<<30:
-		return fmt.Sprintf("%.2fMB", float64(i)/(1<<20))
-	default:
-		return fmt.Sprintf("%.2fGB", float64(i)/(1<<30))
-	}
+type FileInfo struct {
+    Name_    string
+    Size_    int64
+    Mode_    os.FileMode
+    ModTime_ time.Time
 }
 
-type String155 [155]byte
-
-func (s String155) String() string {
-	return string(s[:])
+func (f FileInfo) Name() string {
+    return f.Name_
 }
 
-type String100 [100]byte
-
-func NewString100(s string) String100 {
-	var r String100
-	copy(r[:], s)
-	return r
+func (f FileInfo) Size() int64 {
+    return f.Size_
 }
 
-func (s String100) String() string {
-	return string(s[:])
+func (f FileInfo) Mode() os.FileMode {
+    return f.Mode_
 }
 
-type String32 [32]byte
-
-func (s String32) String() string {
-	return string(s[:])
+func (f FileInfo) ModTime() time.Time {
+    return f.ModTime_
 }
 
-type String8 [8]byte
-
-func (s String8) String() string {
-	return string(s[:])
+func (f FileInfo) IsDir() bool {
+    return f.Mode().IsDir()
 }
 
-type Mode [8]byte
-
-func NewMode(mode os.FileMode) Mode {
-	var i uint32
-
-	i = uint32(mode & os.ModePerm)
-	if mode&os.ModeSetuid != 0 {
-		i |= 04000
-	}
-	if mode&os.ModeSetgid != 0 {
-		i |= 02000
-	}
-
-	var m Mode
-	copy(m[:], fmt.Sprintf("%07o", i))
-	return m
+func (f FileInfo) Sys() interface{} {
+    return nil
 }
 
-func (m Mode) FileMode() os.FileMode {
-	var i os.FileMode
-
-	fmt.Sscanf(string(m[3:]), "%o", &i)
-
-	if i&04000 != 0 {
-		i |= os.ModeSetuid
-	}
-	if i&02000 != 0 {
-		i |= os.ModeSetgid
-	}
-
-	return os.FileMode(i)
+type Header struct {
+    HeaderBlock
 }
 
-func (m Mode) String() string {
-	return fmt.Sprintf("%o", m.FileMode())
+func NewHeader(info os.FileInfo) (*Header, error) {
+    h, err := NewHeaderBlock(info)
+	return &Header{h}, err
 }
 
-type Size [12]byte
-
-func NewSize(size uint64) Size {
-	var s Size
-	copy(s[:], []byte(fmt.Sprintf("%07o", size)))
-	return s
-}
-
-func (s Size) Int() int64 {
-	var i int64
-	fmt.Sscanf(string(s[:]), "%o", &i)
-	return i
-}
-
-func (s Size) String() string {
-	return toHumanReadable(s.Int())
-}
-
-type Timestamp [12]byte
-
-func (t Timestamp) Time() time.Time {
-	var i int64
-	fmt.Sscanf(string(t[:]), "%o", &i)
-	return time.Unix(i, 0)
-}
-
-func (t Timestamp) String() string {
-	return fmt.Sprint(t.Time())
-}
-
-type CheckSum [8]byte
-
-func NewCheckSum(checksum int64) CheckSum {
-	var c CheckSum
-	copy(c[:], []byte(fmt.Sprintf("%07o", checksum)))
-	return c
-}
-
-func (c CheckSum) Int() int64 {
-	var i int64
-	fmt.Sscanf(string(c[:]), "%o", &i)
-	return i
-}
-
-func (c CheckSum) String() string {
-	return fmt.Sprintf("0x%016X", c.Int())
-}
-
-type TypeFlag byte
-
-const (
-	REGTYPE  TypeFlag = '0'
-	AREGTYPE TypeFlag = '\n'
-	LINKTYPE TypeFlag = '1'
-	SYMTYPE  TypeFlag = '2'
-	CHRTYPE  TypeFlag = '3'
-	BLKTYPE  TypeFlag = '4'
-	DIRTYPE  TypeFlag = '5'
-	FIFOTYPE TypeFlag = '6'
-	CONTTYPE TypeFlag = '7'
-)
-
-func (t TypeFlag) FileMode() os.FileMode {
-	switch t {
-	case SYMTYPE:
-		return os.ModeSymlink
-	case CHRTYPE:
-		return os.ModeCharDevice
-	case BLKTYPE:
-		return os.ModeDevice
-	case DIRTYPE:
-		return os.ModeDir
-	case FIFOTYPE:
-		return os.ModeNamedPipe
-	default:
-		return os.FileMode(0)
-	}
-}
-
-func (t TypeFlag) String() string {
-	switch t {
-	case REGTYPE:
-		return "regular file"
-	case AREGTYPE:
-		return "regular file (already archived)"
-	case LINKTYPE:
-		return "link"
-	case SYMTYPE:
-		return "symbolic link"
-	case CHRTYPE:
-		return "character device"
-	case BLKTYPE:
-		return "block device"
-	case DIRTYPE:
-		return "directory"
-	case FIFOTYPE:
-		return "fifo special file"
-	case CONTTYPE:
-		return "reserved"
-	default:
-		return "unknown"
-	}
-}
-
-type Magic [6]byte
-
-func NewMagic() Magic {
-	var m Magic
-	copy(m[:], "ustar")
-	return m
-}
-
-func (m Magic) String() string {
-	return string(m[:])
-}
-
-type Version [2]byte
-
-func NewVersion() Version {
-	var v Version
-	v[0] = '0'
-	v[1] = '0'
-	return v
-}
-
-type Block interface {
-	IsHeader() bool
-	IsFooter() bool
-	WriteTo(io.Writer) error
-}
-
-type ID [8]byte
-
-func NewID(id uint32) ID {
-	var i ID
-	copy(i[:], []byte(fmt.Sprintf("%07o", id)))
-	return i
-}
-
-func (id ID) Int() uint32 {
-	var i uint32
-	fmt.Sscanf(string(id[:]), "%o", &i)
-	return i
-}
-
-func (id ID) String() string {
-	return fmt.Sprintf("%d", id.Int())
-}
-
-type HeaderBlock struct {
-	BaseName String100
-	RawMode  Mode
-	UID      ID
-	GID      ID
-	RawSize  Size
-	MTime    Timestamp
-	CheckSum CheckSum
-	TypeFlag TypeFlag
-	LinkName String100
-	Magic    Magic
-	Version  Version
-	UName    String32
-	GName    String32
-	DevMajor String8
-	DevMinor String8
-	Prefix   String155
-	Padding  [12]byte
-}
-
-func (h HeaderBlock) IsHeader() bool {
-	return true
-}
-
-func (h HeaderBlock) IsFooter() bool {
-	return false
-}
-
-func (h HeaderBlock) WriteTo(w io.Writer) error {
-	return binary.Write(w, binary.BigEndian, h)
-}
-
-func (h HeaderBlock) Bytes() []byte {
-	buf := new(bytes.Buffer)
-	h.WriteTo(buf)
-	return buf.Bytes()
-}
-
-func (h HeaderBlock) calcTotal() int64 {
-	var sum int64
-
-	for _, x := range h.Bytes() {
-		sum += int64(x)
-	}
-
-	return sum
-}
-
-func (h HeaderBlock) CalcSum() int64 {
-	sum := h.calcTotal()
-
-	for _, x := range [8]byte(h.CheckSum) {
-		sum -= int64(x)
-	}
-	sum += 0x30*6 - 0x20
-
-	return sum
-}
-
-func (h HeaderBlock) Validate() bool {
-	return h.CalcSum() == h.CheckSum.Int()
-}
-
-func (h HeaderBlock) Name() string {
-	pre := h.Prefix.String()
+func (h Header) Name() string {
+	pre := h.HeaderBlock.Prefix.String()
 	if pre == "" {
-		return h.BaseName.String()
+		return h.HeaderBlock.Name.String()
 	} else {
-		return fmt.Sprintf("%s/%s", pre, h.BaseName)
+		return fmt.Sprintf("%s/%s", pre, h.HeaderBlock.Name)
 	}
 }
 
-func (h HeaderBlock) Size() int64 {
-	return h.RawSize.Int()
+func (h Header) Size() int64 {
+	return int64(h.HeaderBlock.Size.Int())
 }
 
-func (h HeaderBlock) Mode() os.FileMode {
-	return h.RawMode.FileMode() | h.TypeFlag.FileMode()
+func (h *Header) SetSize(size int64) {
+    h.HeaderBlock.Size = NewSize(uint64(size))
 }
 
-func (h HeaderBlock) ModTime() time.Time {
-	return h.MTime.Time()
+func (h Header) Mode() os.FileMode {
+	return h.HeaderBlock.Mode.FileMode() | h.HeaderBlock.TypeFlag.FileMode()
 }
 
-func (h HeaderBlock) IsDir() bool {
-	return h.TypeFlag == DIRTYPE
+func (h Header) ModTime() time.Time {
+	return h.HeaderBlock.Modified.Time()
 }
 
-func (h HeaderBlock) Sys() interface{} {
+func (h Header) IsDir() bool {
+	return h.HeaderBlock.TypeFlag == DIRTYPE
+}
+
+func (h Header) Sys() interface{} {
 	return nil
 }
 
-type ContentBlock [512]byte
-
-func (c ContentBlock) IsHeader() bool {
-	return false
+func (h Header) Validate() bool {
+    return h.HeaderBlock.Validate()
 }
 
-func (c ContentBlock) IsFooter() bool {
-	return false
+func (h Header) IsHeader() bool {
+    return true
 }
 
-func (c ContentBlock) WriteTo(w io.Writer) error {
-	_, err := w.Write(c[:])
-	return err
+func (h Header) IsFooter() bool {
+    return false
 }
 
-type FooterBlock [1024]byte
-
-func (f FooterBlock) IsHeader() bool {
-	return false
+func (h Header) WriteTo(w io.Writer) error {
+    return h.HeaderBlock.WriteTo(w)
 }
 
-func (f FooterBlock) IsFooter() bool {
-	return true
-}
-
-func (f FooterBlock) WriteTo(w io.Writer) error {
-	_, err := w.Write(f[:])
-	return err
-}
-
-type ContentsArray []ContentBlock
-
-func (cs ContentsArray) Bytes() []byte {
-	var b []byte
-	for _, c := range cs {
-		b = append(b, c[:]...)
-	}
-	return b
-}
-
-func (cs ContentsArray) WriteTo(w io.Writer) error {
-	for _, c := range cs {
-		if _, err := w.Write(c[:]); err != nil {
-			return err
-		}
-	}
-	return nil
+func (h *Header) UpdateSum() {
+    h.HeaderBlock.CheckSum = NewCheckSum(h.HeaderBlock.CalcSum())
 }
 
 type File struct {
-	Header HeaderBlock
-	Body   []byte
+	Header *Header
+	body   []byte
 	reader io.ReadSeeker
 }
 
-func NewFile(name string) *File {
+func NewFile(info os.FileInfo) (*File, error) {
 	body := []byte{}
 
-	h := HeaderBlock{
-		BaseName: NewString100(name),
-		RawMode:  NewMode(os.ModePerm),
-		TypeFlag: REGTYPE,
-		UID:      NewID(1000),
-		GID:      NewID(1000),
-		Magic:    NewMagic(),
-		Version:  NewVersion(),
-	}
-
-	h.CheckSum = NewCheckSum(h.CalcSum())
-
+    h, err := NewHeader(info)
 	return &File{
 		Header: h,
-		Body:   body,
+		body:   body,
 		reader: bytes.NewReader(body),
-	}
+	}, err
 }
 
-func ParseFile(r io.Reader) (*File, error) {
-	f := File{}
+func NewFileFromBinary(r io.Reader) (*File, error) {
+	f := File{Header: new(Header)}
 
-	if err := binary.Read(r, binary.BigEndian, &f.Header); err != nil {
+	if err := binary.Read(r, binary.BigEndian, f.Header); err != nil {
 		return nil, err
 	}
 
-	if f.Header.calcTotal() == 0 {
+	if f.Header.HeaderBlock.IsFooter() {
 		return nil, io.EOF
 	}
 
-	if f.Header.TypeFlag != REGTYPE && f.Header.TypeFlag != CONTTYPE {
-		return &f, nil
-	}
-
-	blocks := (f.Header.Size() + 511) / 512
+	blocks := int64(f.Header.HeaderBlock.ContentBlockNum())
+    if blocks == 0 {
+        return &f, nil
+    }
 
 	buf := bytes.NewBuffer([]byte{})
-	if n, err := io.CopyN(buf, r, blocks); err != nil {
+	if n, err := io.CopyN(buf, r, blocks * 512); err != nil {
 		return nil, err
-	} else if n != blocks {
+	} else if n != (blocks * 512) {
 		return nil, io.EOF
 	}
 
-	f.Body = buf.Bytes()[:f.Header.Size()]
+	f.body = buf.Bytes()[:f.Header.Size()]
 
 	return &f, nil
 }
@@ -440,12 +154,8 @@ func (f File) Name() string {
 	return f.Header.Name()
 }
 
-func (f File) Size() uint64 {
-	return uint64(len(f.Body))
-}
-
-func (f File) Blocks() int {
-	return int((f.Size() + 511) / 512)
+func (f File) Stat() (os.FileInfo, error) {
+    return f.Header, nil
 }
 
 func (f *File) Write(p []byte) (int, error) {
@@ -454,16 +164,16 @@ func (f *File) Write(p []byte) (int, error) {
 		return 0, err
 	}
 
-	if len(f.Body)-int(pos) < len(p) {
-		buf := make([]byte, len(f.Body)-int(pos)+len(p))
-		copy(buf, f.Body)
-		f.Body = buf
+	if len(f.body)-int(pos) < len(p) {
+		buf := make([]byte, len(f.body)-int(pos)+len(p))
+		copy(buf, f.body)
+		f.body = buf
 	}
 
-	n, err := f.Seek(int64(copy(f.Body[pos:], p)), io.SeekCurrent)
+	n, err := f.Seek(int64(copy(f.body[pos:], p)), io.SeekCurrent)
 
-	f.Header.RawSize = NewSize(f.Size())
-	f.Header.CheckSum = NewCheckSum(f.Header.CalcSum())
+    f.Header.SetSize(int64(len(f.body)))
+    f.Header.UpdateSum()
 
 	return int(n), err
 }
@@ -476,25 +186,19 @@ func (f *File) Seek(offset int64, whence int) (int64, error) {
 	return f.reader.Seek(offset, whence)
 }
 
-func (f File) ContentBlocks() ContentsArray {
-	var arr ContentsArray
-	for i := 0; i < int((f.Size()+511)/512); i++ {
-		var c ContentBlock
-		copy(c[:], f.Body[i*512:])
-		arr = append(arr, c)
-	}
-	return arr
-}
-
 func (f File) WriteTo(w io.Writer) error {
 	if err := f.Header.WriteTo(w); err != nil {
 		return err
 	}
 
-	if f.Header.TypeFlag == REGTYPE || f.Header.TypeFlag == CONTTYPE {
-		if err := f.ContentBlocks().WriteTo(w); err != nil {
+	if f.Header.HeaderBlock.ContentBlockNum() > 0 {
+        if _, err := w.Write(f.body); err != nil {
 			return err
 		}
+
+        if _, err := w.Write(make([]byte, ((len(f.body) + 511) / 512) * 512 - len(f.body))); err != nil {
+            return err
+        }
 	}
 
 	return nil
@@ -502,7 +206,7 @@ func (f File) WriteTo(w io.Writer) error {
 
 func Walk(r io.Reader, fun func(*File) error) error {
 	for {
-		f, err := ParseFile(r)
+		f, err := NewFileFromBinary(r)
 		if err == io.EOF {
 			break
 		} else if err != nil {
@@ -517,9 +221,111 @@ func Walk(r io.Reader, fun func(*File) error) error {
 	return nil
 }
 
+type FileView struct {
+    tar    Tar
+    file   *File
+    reader io.ReadSeeker
+}
+
+func NewFileView(t Tar, f *File) *FileView {
+    return &FileView{
+        tar:    t,
+        file:   f,
+        reader: bytes.NewReader(f.body),
+    }
+}
+
+func (f FileView) Close() error {
+    if f.reader == nil {
+        return io.ErrClosedPipe
+    }
+    f.reader = nil
+    return nil
+}
+
+func (f FileView) Read(p []byte) (n int, err error) {
+    if f.reader == nil {
+        return 0, io.ErrClosedPipe
+    }
+    n, err = f.reader.Read(p)
+    return
+}
+
+func (f FileView) Seek(offset int64, whence int) (int64, error) {
+    if f.reader == nil {
+        return 0, io.ErrClosedPipe
+    }
+    return f.reader.Seek(offset, whence)
+}
+
+func (f FileView) Readdir(count int) ([]os.FileInfo, error) {
+    fs := []os.FileInfo{}
+
+    for _, x := range f.tar {
+        n := path.Clean(x.Name())
+        if !strings.HasSuffix(n, "/") {
+            n += "/"
+        }
+        if strings.HasPrefix(n, path.Clean(f.file.Name())) {
+            s, err := x.Stat()
+            if err != nil {
+                return nil, err
+            }
+            fs = append(fs, s)
+        }
+    }
+
+    return fs[:count], nil
+}
+
+func (f FileView) Stat() (os.FileInfo, error) {
+    return f.file.Stat()
+}
+
+type Tar []*File
+
+func Read(r io.Reader) (Tar, error) {
+    var t Tar
+
+    err := Walk(r, func(f *File) error {
+        t = append(t, f)
+        return nil
+    })
+
+    return t, err
+}
+
+func (t Tar) Open(name string) (http.File, error) {
+    name = path.Clean("." + name)
+    for _, x := range t {
+        if path.Clean(x.Name()) == name {
+            return NewFileView(t, x), nil
+        }
+    }
+    return nil, http.ErrMissingFile
+}
+
 func main() {
+    /*
 	out, _ := os.Create("out.tar")
-	file := NewFile("foobar")
+
+	file, err := NewFile(FileInfo{
+        Name_: "foobar",
+        ModTime_: time.Now(),
+        Mode_: 0644,
+    })
+    if err != nil {
+        panic(err.Error())
+    }
+
 	fmt.Fprintln(file, "hello world!")
 	file.WriteTo(out)
+    */
+
+    in, _ := os.Open("www.tar")
+    t, err := Read(in)
+    if err != nil {
+        panic(err.Error())
+    }
+    http.ListenAndServe("localhost:8080", http.FileServer(t))
 }
